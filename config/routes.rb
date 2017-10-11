d42c9e3dc0a9143d0f506d9f124d409b71a528e4
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-11 15:56:11 +0300

    Add main page and footer for debugginn purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
 
   resources :users, only: [:new, :create]
+  resources :main, only: [:index]
+
   controller :sessions do
     get 'login', to: 'sessions#new'
     post 'sessions', to: 'sessions#create'
