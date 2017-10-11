19fc618295fc28e23d2ce7b9b88904c1a8ebeca9
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-11 12:55:12 +0300

    Add session controller and login form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create]
+  controller :sessions do
+    get 'login', to: 'sessions#new'
+    post 'sessions', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+  end
 end
