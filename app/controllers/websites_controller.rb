592f5553ddd485b43ec443c28185dc7ae4658eed
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-12 15:46:22 +0300

    Add form and logic for website creation

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,11 +1,21 @@
 class WebsitesController < ApplicationController
+  before_action :set_website, only: [:show, :edit, :update, :destroy]
+
   def index
+    @websites = current_user.websites
   end
 
   def new
+    @website = Website.new
   end
 
   def create
+    @website = current_user.websites.build(website_params)
+    if @website.save
+      redirect_to websites_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +28,15 @@ class WebsitesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_website
+    @website = current_user.websites.find(params[:id])
+  end
+
+  def website_params
+    params.require(:website).permit(:name, :domain_name)
   end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,5 @@
 class Website < ApplicationRecord
   belongs_to :user
+
+  validates :name, :domain_name, presence: true
 end
