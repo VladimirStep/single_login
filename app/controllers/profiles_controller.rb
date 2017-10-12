4b8e963b60dcefb388176299a3c9b697373e3709
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-12 12:22:27 +0300

    Add form and CRUD logic for Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,45 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+
   def new
+    @profile = Profile.new
   end
 
-  def edit
+  def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
+    if @profile.update_attributes(profile_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @profile.destroy
+    redirect_to profile_path
   end
 
-  def create
+  private
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :gender, :birth_date, :street, :city, :country)
   end
 end
