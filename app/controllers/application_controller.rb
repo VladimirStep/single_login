3ee0cd5e2b0fadc5a562e1c4444104895a433792
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-13 12:53:30 +0300

    Ensure user auth in current flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user
+
+  def authenticate_user
+    redirect_to login_path unless authenticated?
+  end
 end
