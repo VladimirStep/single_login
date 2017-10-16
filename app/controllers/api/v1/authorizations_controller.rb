76eded8a829f18d72460020f867191f818525180
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-16 09:45:04 +0300

    Refactor website authentification and request for token

--- app/controllers/api/v1/authorizations_controller.rb
+++ app/controllers/api/v1/authorizations_controller.rb
@@ -1,29 +1,30 @@
 class Api::V1::AuthorizationsController < ApplicationController
   skip_before_action :authenticate_user, only: [:request_token, :me]
-  before_action :authenticate_website, only: [:request_token]
 
   def authorize
-    session[:oauth_redirect_uri] = params[:redirect_uri]
-
     website = current_user.websites.find_by_secrete_id(params[:client_id])
-    grant = current_user.granted_accesses.where(website_id: website.id).first_or_initialize
-    grant.state = params[:state]
-
-    if grant.save
-      params = { code: grant.code,
-                 response_type: 'code',
-                 state: grant.state }
-      uri = URI.parse(session[:oauth_redirect_uri])
-      redirect_url = URI::HTTP.build([nil, uri.host, uri.port, uri.path, params.to_query, nil]).to_s
-
-      redirect_to redirect_url
+    if website
+      grant = current_user.granted_accesses.where(website_id: website.id).first_or_initialize
+      grant.state = params[:state]
+      if grant.save
+        new_params = { code: grant.code,
+                   response_type: 'code',
+                   state: grant.state }
+      end
+    else
+      new_params = { state: params[:state] }
     end
+
+    uri = URI.parse(params[:redirect_uri])
+    redirect_url = URI::HTTP.build([nil, uri.host, uri.port, uri.path, new_params.to_query, nil]).to_s
+
+    redirect_to redirect_url
   end
 
   def request_token
-    code = @website&.granted_access&.code
-    if code == params[:code]
-      render json: { access_token: @website.granted_access.access_token }, status: 200
+    website = Website.try_authenticate(params[:client_id], params[:client_secret])
+    if website&.granted_access&.code == params[:code]
+      render json: { access_token: website.granted_access.access_token }, status: 200
     else
       render json: {}, status: 401
     end
@@ -35,11 +36,5 @@ class Api::V1::AuthorizationsController < ApplicationController
 
   def me
     render json: { name: 'Vladimir', email: 'my@email.com', id: '123' }, status: 200 # This is a stub
-  end
-
-  private
-
-  def authenticate_website
-    @website = Website.try_authenticate(params[:client_id], params[:client_secret])
   end
 end
