35d7275baf20e2921cffad6aef275e011765ad89
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-17 14:03:52 +0300

    Add user authorization immidiatly after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      warden.set_user(@user)
       redirect_to root_path
     else
       render 'new'
